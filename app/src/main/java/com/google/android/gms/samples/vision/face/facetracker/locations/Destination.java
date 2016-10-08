cd6f95fc7986d7ad64616509c6fdea8c1b6b5070
GeoffreyLong <geoffreylong@mail.mcgill.ca>
2016-10-08 03:00:43 -0400

    ADD very basic skeleton

---
+++ app/src/main/java/com/google/android/gms/samples/vision/face/facetracker/locations/Destination.java
@@ -0,0 +1,8 @@
+package com.google.android.gms.samples.vision.face.facetracker.locations;
+
+/**
+ * Created by GeoffreyLong on 10/8/2016.
+ */
+
+public class Destination {
+}
---
+++ app/src/main/java/com/google/android/gms/samples/vision/face/facetracker/locations/UserLocation.java
@@ -0,0 +1,26 @@
+package com.google.android.gms.samples.vision.face.facetracker.locations;
+
+/**
+ * Created by GeoffreyLong on 10/8/2016.
+ */
+
+public class UserLocation {
+    double heading;
+    double latitude;
+    double longitude;
+
+    public void getUserLocation() {
+
+
+    }
+
+    private void getCoords() {
+
+
+    }
+
+    private void getCompass() {
+
+
+    }
+}
